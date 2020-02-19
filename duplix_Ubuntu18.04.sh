e3c28492c8fb7863d04c0953528438128cae832b
safexain <52562552+safexain@users.noreply.github.com>
2020-02-19 09:56:37 +0100

    Update duplix_Ubuntu18.04.sh

--- duplix_Ubuntu18.04.sh
+++ duplix_Ubuntu18.04.sh
@@ -3,11 +3,11 @@
 set -eu
 
 # Duplix
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 
-echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+#echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 
-sudo apt update \ sudo apt install mono-devel gtk-sharp2
+#sudo apt update \ sudo apt install mono-devel gtk-sharp2
 
 wget https://github.com/safexain/Duplix/raw/master/duplix-2.0.deb
 
